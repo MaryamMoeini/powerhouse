a93a0a6d9d20262ace9e66277b67d797cbcaaf0a
MaryamMoeini <moeini_maryam2414@yahoo.com>
2017-03-30 11:17:45 +0800

    Add files via upload

---
+++ powerhouse/src/main/java/com/powerhouse/PowerhouseApplication.java
@@ -0,0 +1,12 @@
+package com.powerhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerhouseApplication.class, args);
+	}
+}
